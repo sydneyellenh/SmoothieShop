55989f3082635c0bc83da1f9e8a7199e42aaccd7
Sydney Haggard <sydneyellen1996@gmail.com>
2016-11-16 22:12:11 -0500

    Step3 commit

---
+++ src/main/java/org/elevenfifty/smoothie/Browser.java
@@ -0,0 +1,34 @@
+package org.elevenfifty.smoothie;
+
+import java.util.Scanner;
+
+import org.elevenfifty.smoothie.beans.Recipe;
+
+public class Browser {
+	
+//	Question – What is the displayRecipes method doing to enhance 
+	//the UI of our console app? Elaborate and be specific.  
+	//Make sure to discuss what out.format is doing!
+
+	private final Configuration config;
+	private final Scanner in = new Scanner(System.in);
+
+	public Browser(Configuration config) {
+		this.config = config;
+	}
+
+	public void displayRecipes() {
+		for (int i = 0; i < config.listRecipes().size(); i++) {
+			Recipe r = config.getRecipe(i);
+			System.out.format("%d: %s $%,01.2f%n", i + 1, r.getName(), r.getCost());
+		}
+	}
+
+	public Recipe readRecipe() {
+		System.out.println();
+		System.out.print("Select a Smoothie Recipe: ");
+
+		return config.getRecipe(in.nextInt() - 1);
+	}
+
+}
